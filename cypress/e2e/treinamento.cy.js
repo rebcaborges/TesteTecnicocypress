724bdc1ed8518b0ebb88695c5f2b0c34cce1fc28
bexxborgs <rborges.qa@gmail.com>
2024-12-17 11:54:16 -0300

    Atualiza treinamento.cy.js

--- cypress/e2e/treinamento.cy.js
+++ cypress/e2e/treinamento.cy.js
@@ -1,56 +1,41 @@
-
-
-
 describe('Entradas', () => {
-    it('Cadastrar uma entrada', () => {
+    beforeEach(() => {
         cy.visit("https://www.wcaquino.me/cypress/componentes.html");
-        cy.get("#formNome").should("exist");
-        cy.get('#formNome').type("Maria Lorem ipsum dolor sit ")
-        cy.get('[data-cy="dataSobrenome"]').type("Souza Silva")
-        cy.get('#formSexoFem').click();
-        cy.get('#formComidaVegetariana').click();
-        cy.get('[data-test="dataEscolaridade"]').select('Superior');
-        cy.get('[value="Corrida"]').click();
-        cy.get('#elementosForm\\:sugestoes').type("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.");
-        // Campo Sugestões dá error quando excede quantidadde de letras 
+    });
 
-        cy.get(':nth-child(2) > :nth-child(1) > :nth-child(3) > input');
-        cy.get(':nth-child(2) > :nth-child(1) > :nth-child(4) > input').click();
-        cy.get(':nth-child(1) > :nth-child(5) > table > tbody > tr > td > input').click();
-        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input').type("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.");
+    it('Cadastrar uma entrada', () => {
+        // Carregando dados externos
+        cy.fixture('entradas').then((data) => {
+            // Preenchendo dados principais
+            cy.get("#formNome").should("exist").type(data.nome);
+            cy.get('[data-cy="dataSobrenome"]').type(data.sobrenome);
+            cy.get('#formSexoFem').click();
+            cy.get('#formComidaVegetariana').click();
+            cy.get('[data-test="dataEscolaridade"]').select('Superior');
+            cy.get('[value="Corrida"]').click();
+            cy.get('#elementosForm\\:sugestoes').type(data.sugestao);
 
-        cy.get(':nth-child(2) > :nth-child(3) > input');
-        cy.get(':nth-child(2) > :nth-child(4) > input').click();
-        cy.get(':nth-child(2) > :nth-child(5) > table > tbody > tr > td > input').click();
-        cy.get(':nth-child(2) > :nth-child(6) > input').type("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.");
+            // Validação do campo sugestões
+            cy.get('#elementosForm\\:sugestoes').should('have.value', data.sugestao);
 
-        cy.get(':nth-child(3) > :nth-child(3) > input');
-        cy.get(':nth-child(3) > :nth-child(4) > input').click();
-        cy.get(':nth-child(3) > :nth-child(5) > table > tbody > tr > td > input').click();
-        cy.get(':nth-child(3) > :nth-child(6) > input').type("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.");
+            // Função auxiliar para preencher linhas da tabela
+            const preencherLinha = (indice, texto) => {
+                cy.get(`:nth-child(${indice}) > :nth-child(3) > input`).click();
+                cy.get(`:nth-child(${indice}) > :nth-child(4) > input`).click();
+                cy.get(`:nth-child(${indice}) > :nth-child(5) > table > tbody > tr > td > input`).click();
+                cy.get(`:nth-child(${indice}) > :nth-child(6) > input`).type(texto);
+            };
 
-        cy.get(':nth-child(4) > :nth-child(3) > input');
-        cy.get(':nth-child(4) > :nth-child(4)').click();
-        cy.get(':nth-child(4) > :nth-child(5) > table > tbody > tr > td > input').click();
-        cy.get(':nth-child(4) > :nth-child(6) > input').type("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.");
+            // Iterando sobre linhas da tabela
+            data.linhasTabela.forEach((linha) => {
+                preencherLinha(linha.indice, linha.valor);
+            });
 
-        cy.get(':nth-child(5) > :nth-child(3) > input');
-        cy.get(':nth-child(5) > :nth-child(4) > input').click();
-        cy.get(':nth-child(5) > :nth-child(5) > table > tbody > tr > td > input').click();
-        cy.get(':nth-child(5) > :nth-child(6) > input').type("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.")
-        
-        cy.get('#formCadastrar').click();
+            // Cadastrar
+            cy.get('#formCadastrar').click();
 
-
-
-
-
-
-
-
-
-   
-   
-   
+            // Validação final (exemplo: mensagem de sucesso)
+            cy.contains('Cadastrado!').should('be.visible');
+        });
     });
 });
