0b20d30ecdf11335f60f8314231f38f63416665d
Rebeca Borges <87546461+rebcaborges@users.noreply.github.com>
2025-05-21 09:14:29 -0300

    Update treinamento.cy.js
    ATUALIZAÇÃO TESTE TECNICO MAIO DE 2025

--- cypress/e2e/treinamento.cy.js
+++ cypress/e2e/treinamento.cy.js
@@ -4,37 +4,36 @@ describe('Entradas', () => {
     });
 
     it('Cadastrar uma entrada', () => {
-        // Carregando dados externos
         cy.fixture('entradas').then((data) => {
-            // Preenchendo dados principais
-            cy.get("#formNome").should("exist").type(data.nome);
-            cy.get('[data-cy="dataSobrenome"]').type(data.sobrenome);
+            cy.get("#formNome").should("exist").type("Maria");
+            cy.get('[data-cy="dataSobrenome"]').type("Silva Souza");
             cy.get('#formSexoFem').click();
             cy.get('#formComidaVegetariana').click();
             cy.get('[data-test="dataEscolaridade"]').select('Superior');
             cy.get('[value="Corrida"]').click();
             cy.get('#elementosForm\\:sugestoes').type(data.sugestao);
 
-            // Validação do campo sugestões
+
+
             cy.get('#elementosForm\\:sugestoes').should('have.value', data.sugestao);
 
-            // Função auxiliar para preencher linhas da tabela
             const preencherLinha = (indice, texto) => {
-                cy.get(`:nth-child(${indice}) > :nth-child(3) > input`).click();
-                cy.get(`:nth-child(${indice}) > :nth-child(4) > input`).click();
-                cy.get(`:nth-child(${indice}) > :nth-child(5) > table > tbody > tr > td > input`).click();
-                cy.get(`:nth-child(${indice}) > :nth-child(6) > input`).type(texto);
+                const linha = `tbody > tr:nth-child(${indice + 1})`;
+                const valores = ['Teste 01', 'Teste 02', 'Teste 03', 'Teste 04'];
+                valores.forEach((valor, i) => {
+                cy.get(`#tabelaUsuarios > tbody > tr:nth-child(${i + 1}) > td:nth-child(6) > input`)
+                .clear()
+                .type(valor);
+                });             
+              
             };
 
-            // Iterando sobre linhas da tabela
             data.linhasTabela.forEach((linha) => {
                 preencherLinha(linha.indice, linha.valor);
             });
 
-            // Cadastrar
             cy.get('#formCadastrar').click();
 
-            // Validação final (exemplo: mensagem de sucesso)
             cy.contains('Cadastrado!').should('be.visible');
         });
     });
